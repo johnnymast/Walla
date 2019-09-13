e391923ff59ea1f4bf306b01236d2be52ad2a57a
Johnny Mast <mastjohnny@gmail.com>
2019-09-13 15:04:28 +0200

    Helpers and Geometry now have the namespaces as well.

--- output/build.js
+++ output/build.js
@@ -79006,7 +79006,7 @@ module.exports = SceneManager;
 /* 369 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const LocalStorage = __webpack_require__(395);
+const LocalStorage = __webpack_require__(618);
 
 /**
  * StateManager
@@ -81412,55 +81412,7 @@ if (true) {
 }
 
 /***/ }),
-/* 395 */
-/***/ (function(module, exports) {
-
-class LocalStorage {
-
-  /**
-   * @classdesc LocalStorage
-   * @exports  core/storage/LocalStorage
-   * @class
-   */
-  constructor() {
-    if (typeof Storage === 'undefined') {
-      throw new Error('LocalStorage: localStorage is not suppored by this browser.');
-    }
-  }
-
-  /**
-   * Return the value stored in LocalStorage.
-   *
-   * @param {string} key - Return the value of a stored item with this identifier.
-   * @returns {string}
-   */
-  get(key = '') {
-    return localStorage.getItem(key);
-  }
-
-  /**
-   * Set a value in LocalStorage.
-   *
-   * @param {string} key - The identifier for the value.
-   * @param {*} value - The value to store.
-   */
-  set(key = '', value = '') {
-    return localStorage.setItem(key, value);
-  }
-
-  /**
-   * Unset a value in LocalStorage.
-   *
-   * @param {string} key - The identifier to unset.
-   */
-  unset(key = '') {
-    return localStorage.removeItem(key);
-  }
-}
-
-module.exports = LocalStorage;
-
-/***/ }),
+/* 395 */,
 /* 396 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -83783,6 +83735,55 @@ class Gameloop {
 }
 
 module.exports = Gameloop;
+
+/***/ }),
+/* 618 */
+/***/ (function(module, exports) {
+
+class LocalStorage {
+
+  /**
+   * @classdesc LocalStorage
+   * @exports  core/storage/LocalStorage
+   * @class
+   */
+  constructor() {
+    if (typeof Storage === 'undefined') {
+      throw new Error('LocalStorage: localStorage is not suppored by this browser.');
+    }
+  }
+
+  /**
+   * Return the value stored in LocalStorage.
+   *
+   * @param {string} key - Return the value of a stored item with this identifier.
+   * @returns {string}
+   */
+  get(key = '') {
+    return localStorage.getItem(key);
+  }
+
+  /**
+   * Set a value in LocalStorage.
+   *
+   * @param {string} key - The identifier for the value.
+   * @param {*} value - The value to store.
+   */
+  set(key = '', value = '') {
+    return localStorage.setItem(key, value);
+  }
+
+  /**
+   * Unset a value in LocalStorage.
+   *
+   * @param {string} key - The identifier to unset.
+   */
+  unset(key = '') {
+    return localStorage.removeItem(key);
+  }
+}
+
+module.exports = LocalStorage;
 
 /***/ })
 /******/ ]);
